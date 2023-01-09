6c6873742d9a6a48dfb3a0d0c22b2ac5aafd75cd
Ruyther <ruy_fusion@hotmail.com>
2023-01-09 17:33:43 -0300

    Initial commit

---
+++ DigioStore/AppDelegate.swift
@@ -0,0 +1,16 @@
+//
+//  AppDelegate.swift
+//  DigioStore
+//
+//  Created by Ruyther Costa on 09/01/23.
+//
+
+import UIKit
+
+@main
+final class AppDelegate: UIResponder, UIApplicationDelegate {
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+}
---
+++ DigioStoreTests/DigioStoreTests.swift
@@ -0,0 +1,19 @@
+//
+//  DigioStoreTests.swift
+//  DigioStoreTests
+//
+//  Created by Ruyther Costa on 09/01/23.
+//
+
+import XCTest
+@testable import DigioStore
+
+final class DigioStoreTests: XCTestCase {
+    override func setUp() {
+        super.setUp()
+    }
+
+    override func tearDown() {
+        super.tearDown()
+    }
+}
